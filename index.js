3b212b847f170c7215704d9f3516373763512170
Joshua Cook <jcook@lynxsolutions.com>
2018-11-04 16:40:15 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+
+const server = createServer();
+
+// TODO Use express middlware to handle cookies (JWT)
+// TODO Use express middlware to populate current user
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
+        },
+    },
+    deets => {
+        console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    }
+);
